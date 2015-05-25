6130d0a22b328bcaeda68a7c714217b751562f3b
Hieu Hoang <hieuhoang@gmail.com>
2015-05-25 15:42:54 +0400

    add option to stem words in sentences, rather than 1 word-per-line

--- examples/stemwords.c
+++ examples/stemwords.c
@@ -1,7 +1,6 @@
 /* This is a simple program which uses libstemmer to provide a command
  * line interface for stemming using any of the algorithms provided.
  */
-
 #include <stdio.h>
 #include <stdlib.h> /* for malloc, free */
 #include <string.h> /* for memmove */
@@ -22,61 +21,62 @@ stem_file(struct sb_stemmer * stemmer, FILE * f_in, FILE * f_out)
     while(1) {
         int ch = getc(f_in);
         if (ch == EOF) {
-            free(b); return;
+            free(b);
+            return;
         }
         {
             int i = 0;
-	    int inlen = 0;
+            int inlen = 0;
             while(1) {
                 if (ch == '\n' || ch == EOF) break;
                 if (i == lim) {
                     sb_symbol * newb;
-		    newb = (sb_symbol *)
-			    realloc(b, (lim + INC) * sizeof(sb_symbol));
-		    if (newb == 0) goto error;
-		    b = newb;
+                    newb = (sb_symbol *)
+                           realloc(b, (lim + INC) * sizeof(sb_symbol));
+                    if (newb == 0) goto error;
+                    b = newb;
                     lim = lim + INC;
                 }
-		/* Update count of utf-8 characters. */
-		if (ch < 0x80 || ch > 0xBF) inlen += 1;
+                /* Update count of utf-8 characters. */
+                if (ch < 0x80 || ch > 0xBF) inlen += 1;
                 /* force lower case: */
                 if (isupper(ch)) ch = tolower(ch);
 
                 b[i] = ch;
-		i++;
+                i++;
                 ch = getc(f_in);
             }
 
-	    {
-		const sb_symbol * stemmed = sb_stemmer_stem(stemmer, b, i);
+            {
+                const sb_symbol * stemmed = sb_stemmer_stem(stemmer, b, i);
                 if (stemmed == NULL)
                 {
                     fprintf(stderr, "Out of memory");
                     exit(1);
                 }
                 else
-		{
-		    if (pretty == 1) {
-			fwrite(b, i, 1, f_out);
-			fputs(" -> ", f_out);
-		    } else if (pretty == 2) {
-			fwrite(b, i, 1, f_out);
-			if (sb_stemmer_length(stemmer) > 0) {
-			    int j;
-			    if (inlen < 30) {
-				for (j = 30 - inlen; j > 0; j--)
-				    fputs(" ", f_out);
-			    } else {
-				fputs("\n", f_out);
-				for (j = 30; j > 0; j--)
-				    fputs(" ", f_out);
-			    }
-			}
-		    }
+                {
+                    if (pretty == 1) {
+                        fwrite(b, i, 1, f_out);
+                        fputs(" -> ", f_out);
+                    } else if (pretty == 2) {
+                        fwrite(b, i, 1, f_out);
+                        if (sb_stemmer_length(stemmer) > 0) {
+                            int j;
+                            if (inlen < 30) {
+                                for (j = 30 - inlen; j > 0; j--)
+                                    fputs(" ", f_out);
+                            } else {
+                                fputs("\n", f_out);
+                                for (j = 30; j > 0; j--)
+                                    fputs(" ", f_out);
+                            }
+                        }
+                    }
 
-		    fputs((const char *)stemmed, f_out);
-		    putc('\n', f_out);
-		}
+                    fputs((char *)stemmed, f_out);
+                    putc('\n', f_out);
+                }
             }
         }
     }
@@ -92,25 +92,25 @@ static void
 usage(int n)
 {
     printf("usage: %s [-l <language>] [-i <input file>] [-o <output file>] [-c <character encoding>] [-p[2]] [-h]\n"
-	  "\n"
-	  "The input file consists of a list of words to be stemmed, one per\n"
-	  "line. Words should be in lower case, but (for English) A-Z letters\n"
-	  "are mapped to their a-z equivalents anyway. If omitted, stdin is\n"
-	  "used.\n"
-	  "\n"
-	  "If -c is given, the argument is the character encoding of the input\n"
-          "and output files.  If it is omitted, the UTF-8 encoding is used.\n"
-	  "\n"
-	  "If -p is given the output file consists of each word of the input\n"
-	  "file followed by \"->\" followed by its stemmed equivalent.\n"
-	  "If -p2 is given the output file is a two column layout containing\n"
-	  "the input words in the first column and the stemmed equivalents in\n"
-	  "the second column.\n"
-	  "Otherwise, the output file consists of the stemmed words, one per\n"
-	  "line.\n"
-	  "\n"
-	  "-h displays this help\n",
-	  progname);
+           "\n"
+           "The input file consists of a list of words to be stemmed, one per\n"
+           "line. Words should be in lower case, but (for English) A-Z letters\n"
+           "are mapped to their a-z equivalents anyway. If omitted, stdin is\n"
+           "used.\n"
+           "\n"
+           "If -c is given, the argument is the character encoding of the input\n"
+           "and output files.  If it is omitted, the UTF-8 encoding is used.\n"
+           "\n"
+           "If -p is given the output file consists of each word of the input\n"
+           "file followed by \"->\" followed by its stemmed equivalent.\n"
+           "If -p2 is given the output file is a two column layout containing\n"
+           "the input words in the first column and the stemmed eqivalents in\n"
+           "the second column.\n"
+           "Otherwise, the output file consists of the stemmed words, one per\n"
+           "line.\n"
+           "\n"
+           "-h displays this help\n",
+           progname);
     exit(n);
 }
 
@@ -125,7 +125,8 @@ main(int argc, char * argv[])
 
     char * language = "english";
     char * charenc = NULL;
-
+		char sentences = 0;
+		
     char * s;
     int i = 1;
     pretty = 0;
@@ -133,58 +134,60 @@ main(int argc, char * argv[])
     progname = argv[0];
 
     while(i < argc) {
-	s = argv[i++];
-	if (s[0] == '-') {
-	    if (strcmp(s, "-o") == 0) {
-		if (i >= argc) {
-		    fprintf(stderr, "%s requires an argument\n", s);
-		    exit(1);
-		}
-		out = argv[i++];
-	    } else if (strcmp(s, "-i") == 0) {
-		if (i >= argc) {
-		    fprintf(stderr, "%s requires an argument\n", s);
-		    exit(1);
-		}
-		in = argv[i++];
-	    } else if (strcmp(s, "-l") == 0) {
-		if (i >= argc) {
-		    fprintf(stderr, "%s requires an argument\n", s);
-		    exit(1);
-		}
-		language = argv[i++];
-	    } else if (strcmp(s, "-c") == 0) {
-		if (i >= argc) {
-		    fprintf(stderr, "%s requires an argument\n", s);
-		    exit(1);
-		}
-		charenc = argv[i++];
-	    } else if (strcmp(s, "-p2") == 0) {
-		pretty = 2;
-	    } else if (strcmp(s, "-p") == 0) {
-		pretty = 1;
-	    } else if (strcmp(s, "-h") == 0) {
-		usage(0);
-	    } else {
-		fprintf(stderr, "option %s unknown\n", s);
-		usage(1);
-	    }
-	} else {
-	    fprintf(stderr, "unexpected parameter %s\n", s);
-	    usage(1);
-	}
+        s = argv[i++];
+        if (s[0] == '-') {
+            if (strcmp(s, "-o") == 0) {
+                if (i >= argc) {
+                    fprintf(stderr, "%s requires an argument\n", s);
+                    exit(1);
+                }
+                out = argv[i++];
+            } else if (strcmp(s, "-i") == 0) {
+                if (i >= argc) {
+                    fprintf(stderr, "%s requires an argument\n", s);
+                    exit(1);
+                }
+                in = argv[i++];
+            } else if (strcmp(s, "-l") == 0) {
+                if (i >= argc) {
+                    fprintf(stderr, "%s requires an argument\n", s);
+                    exit(1);
+                }
+                language = argv[i++];
+            } else if (strcmp(s, "-c") == 0) {
+                if (i >= argc) {
+                    fprintf(stderr, "%s requires an argument\n", s);
+                    exit(1);
+                }
+                charenc = argv[i++];
+            } else if (strcmp(s, "-p2") == 0) {
+                pretty = 2;
+            } else if (strcmp(s, "-p") == 0) {
+                pretty = 1;
+		        } else if (strcmp(s, "-s") == 0) {
+            		sentences = 1;
+            } else if (strcmp(s, "-h") == 0) {
+                usage(0);
+            } else {
+                fprintf(stderr, "option %s unknown\n", s);
+                usage(1);
+            }
+        } else {
+            fprintf(stderr, "unexpected parameter %s\n", s);
+            usage(1);
+        }
     }
 
     /* prepare the files */
     f_in = (in == 0) ? stdin : fopen(in, "r");
     if (f_in == 0) {
-	fprintf(stderr, "file %s not found\n", in);
-	exit(1);
+        fprintf(stderr, "file %s not found\n", in);
+        exit(1);
     }
     f_out = (out == 0) ? stdout : fopen(out, "w");
     if (f_out == 0) {
-	fprintf(stderr, "file %s cannot be opened\n", out);
-	exit(1);
+        fprintf(stderr, "file %s cannot be opened\n", out);
+        exit(1);
     }
 
     /* do the stemming process: */
@@ -198,7 +201,66 @@ main(int argc, char * argv[])
             exit(1);
         }
     }
-    stem_file(stemmer, f_in, f_out);
+    
+    if (sentences) {
+      char * line = NULL;
+      size_t len = 0;
+      ssize_t read;
+       
+      while ((read = getline(&line, &len, f_in)) != -1) {
+        //printf("Retrieved line of length %zu :\n", read);
+        //printf("%s", line);
+
+				// temp file name
+				char inPath[L_tmpnam]; // = "/tmp/in";
+				char outPath[L_tmpnam]; // = "/tmp/out";
+  			tmpnam (inPath);
+  			tmpnam (outPath);
+  
+				FILE *inFile;
+				FILE *outFile;
+				
+				// put 1 sentence into a file
+				inFile = fopen(inPath, "w");
+  			char* token = strtok(line, " ");
+				while (token) {
+						fprintf(inFile,  "%s\n", token);
+						token = strtok(NULL, " ");
+				}			
+				fclose(inFile);
+
+				// stemming
+				inFile = fopen(inPath, "r");
+				outFile = fopen(outPath, "w");
+					
+				stem_file(stemmer, inFile, outFile);
+
+				fclose(inFile);
+				fclose(outFile);
+				
+				// output
+				outFile = fopen(outPath, "r");
+				 
+				do {
+					char c = fgetc(outFile);
+					if (feof(outFile)) {
+					  fprintf(f_out, "\n");
+					  break;
+					}
+					else if (c == '\n') {
+					  fprintf(f_out, " ");
+					}
+					else {
+						fprintf(f_out, "%c", c);
+					}
+				} while(1);
+      }
+       
+    }
+    else {
+	    stem_file(stemmer, f_in, f_out);
+  	}
+  
     sb_stemmer_delete(stemmer);
 
     if (in != 0) (void) fclose(f_in);
